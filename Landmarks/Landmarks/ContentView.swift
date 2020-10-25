3904d912bf5bb34e28176ee7020351392a28cb55
rainy-me <github@rainy.me>
2020-10-26 06:23:35 +0900

    init

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by tg-mac-pro-05 on 2020/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
